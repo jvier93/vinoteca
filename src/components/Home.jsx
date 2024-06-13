a5bb539a509cea8506a745b2709d876f1c9c3332
Pablo <pablo-krai@hotmail.com>
2024-06-13 20:57:57 -0300

    Accordion items creados segun catalogo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,22 @@ function Home() {
       <div className="py-8  px-2 mx-auto max-w-[800px]">
         <h1 className="mb-8  text-white font-inter pl-4 text-2xl">Catálogo</h1>
         <Accordian>
+          <AccordianItem value="1" category="CASCO VIEJO"></AccordianItem>
+
+          <AccordianItem value="2" category="VIÑA LAS BRUJAS"></AccordianItem>
           <AccordianItem
-            value="1"
-            category="BUHO MICROVINIFICACIONES"
+            value="3"
+            category="100 AÑOS RESERVA FAMILIAR"
           ></AccordianItem>
-          <AccordianItem value="2" category="CASCO VIEJO"></AccordianItem>
-          <AccordianItem value="3" category="VIÑA LAS BRUJAS"></AccordianItem>
+          <AccordianItem
+            value="4"
+            category="GIMENEZ MENDEZ ALTA RESERVA"
+          ></AccordianItem>
+
+          <AccordianItem
+            value="5"
+            category="GIMENEZ MENDEZ RESERVAS ESPECIALES"
+          ></AccordianItem>
         </Accordian>
       </div>
     </div>
